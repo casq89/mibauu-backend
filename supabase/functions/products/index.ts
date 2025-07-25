733ce92f5627f6af2a2edfaa91c1d33457680d15
adiaz <hallpipe10@gmail.com>
2025-07-24 21:41:33 -0500

    feat: is added option to delete images when the product is deleted and is added prettier for format files

--- supabase/functions/products/index.ts
+++ supabase/functions/products/index.ts
@@ -1,39 +1,39 @@
-import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
-import { createClient } from 'jsr:@supabase/supabase-js@2';
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
 import {
   getIdFromUrl,
   sendErrorResponse,
   sendSuccessResponse,
-} from '../../utils/urlUtils.ts';
-import { v4 } from 'https://esm.sh/v135/uuid@9.0.1/es2022/uuid.mjs';
+} from "../../utils/urlUtils.ts";
+import { v4 } from "https://esm.sh/v135/uuid@9.0.1/es2022/uuid.mjs";
 
 const supabase = createClient(
-  Deno.env.get('SUPABASE_URL') ?? '',
-  Deno.env.get('SUPABASE_ANON_KEY') ?? ''
+  Deno.env.get("SUPABASE_URL") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
 );
 
 Deno.serve((req) => {
   try {
     switch (req.method) {
-      case 'GET': {
+      case "GET": {
         return getProducts(req);
       }
-      case 'POST': {
+      case "POST": {
         return postProduct(req);
       }
-      case 'PUT': {
+      case "PUT": {
         return putProduct(req);
       }
-      case 'DELETE': {
+      case "DELETE": {
         return deleteProduct(req);
       }
       default:
         return new Response(
-          JSON.stringify({ error: 'Method is not allowed' }),
+          JSON.stringify({ error: "Method is not allowed" }),
           {
             status: 405,
-            headers: { Allow: 'GET, POST, PUT' },
-          }
+            headers: { Allow: "GET, POST, PUT" },
+          },
         );
     }
   } catch (err: any) {
@@ -43,10 +43,10 @@ Deno.serve((req) => {
       }),
       {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         status: 500,
-      }
+      },
     );
   }
 });
@@ -55,17 +55,17 @@ const getProducts = async (req: Request) => {
   const id = getIdFromUrl(req);
   if (id === undefined) {
     const { data, error } = await supabase
-      .from('products')
-      .select('*, category(name)');
+      .from("products")
+      .select("*, category(name)");
     if (error) {
       throw error;
     }
     return sendSuccessResponse(data);
   } else {
     const { data, error } = await supabase
-      .from('products')
-      .select('*, category(name)')
-      .eq('id', id);
+      .from("products")
+      .select("*, category(name)")
+      .eq("id", id);
     if (error) {
       throw error;
     }
@@ -75,42 +75,40 @@ const getProducts = async (req: Request) => {
 
 const postProduct = async (req: Request) => {
   const formData = await req.formData();
-  const image = formData.get('image') as File;
-  const name = formData.get('name') as string;
-  const description = formData.get('description') as string;
-  const price = parseFloat(formData.get('price') as string);
-  const stock = parseInt(formData.get('stock') as string);
-  const category = parseInt(formData.get('category_id') as string);
-  const promotion = formData.get('promotion') === 'true';
-  const disccount = parseFloat(formData.get('disccount') as string);
-  const enable = formData.get('enable') === 'true';
+  const image = formData.get("image") as File;
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
+  const price = parseFloat(formData.get("price") as string);
+  const stock = parseInt(formData.get("stock") as string);
+  const category = parseInt(formData.get("category_id") as string);
+  const promotion = formData.get("promotion") === "true";
+  const disccount = parseFloat(formData.get("disccount") as string);
+  const enable = formData.get("enable") === "true";
 
-  const fileExt = image.name.split('.').pop();
+  const fileExt = image.name.split(".").pop();
   const fileName = `${v4()}.${fileExt}`;
   const filePath = `${fileName}`;
 
-  const { data: storageData, error: storageError } = await supabase.storage
-    .from('mibauu')
+  const { error: storageError } = await supabase.storage
+    .from("mibauu")
     .upload(filePath, image, {
-      cacheControl: '3600',
+      cacheControl: "3600",
       upsert: false,
     });
 
   if (storageError) return sendErrorResponse(storageError.message);
 
   const { data: urlData } = supabase.storage
-    .from('mibauu')
+    .from("mibauu")
     .getPublicUrl(filePath);
 
   const imageUrl = urlData.publicUrl;
 
-  const body = await req.json();
-
-  const { data: code, errorCode } = await supabase.rpc('get_next_product_code');
+  const { data: code, errorCode } = await supabase.rpc("get_next_product_code");
   if (errorCode) return sendErrorResponse(errorCode.message);
 
   const { data, error } = await supabase
-    .from('products')
+    .from("products")
     .insert([
       {
         code,
@@ -136,12 +134,12 @@ const putProduct = async (req: Request) => {
   const id = getIdFromUrl(req);
   const body = await req.json();
 
-  if (!id) return sendErrorResponse('id is required to update product');
+  if (!id) return sendErrorResponse("id is required to update product");
 
   const { data: product, error: errorProduct } = await supabase
-    .from('products')
-    .select('id')
-    .eq('id', id);
+    .from("products")
+    .select("id")
+    .eq("id", id);
 
   if (errorProduct) {
     throw errorProduct;
@@ -152,9 +150,9 @@ const putProduct = async (req: Request) => {
   }
 
   const { data, error } = await supabase
-    .from('products')
+    .from("products")
     .update(body)
-    .eq('id', id)
+    .eq("id", id)
     .select();
 
   if (error) return sendErrorResponse(error.message);
@@ -165,13 +163,12 @@ const putProduct = async (req: Request) => {
 const deleteProduct = async (req: Request) => {
   const id = getIdFromUrl(req);
 
-  if (!id) return sendErrorResponse('id is required to delete product');
+  if (!id) return sendErrorResponse("id is required to delete product");
 
   const { data: product, error: errorProduct } = await supabase
-    .from('products')
-    .select('id')
-    .eq('id', id);
-
+    .from("products")
+    .select("id, imagen_url")
+    .eq("id", id);
   if (errorProduct) {
     throw errorProduct;
   }
@@ -179,7 +176,25 @@ const deleteProduct = async (req: Request) => {
   if (!product.length) {
     return sendErrorResponse(`Product id: ${id} does not exist`);
   }
-  const { error } = await supabase.from('products').delete().eq('id', id);
+
+  const imageUrl = product[0].imagen_url;
+
+  const bucketName = "mibauu";
+  const path = imageUrl.split(`/storage/v1/object/public/${bucketName}/`)[1];
+
+  if (path) {
+    const { error: deleteImgError } = await supabase.storage
+      .from(bucketName)
+      .remove([path]);
+
+    if (deleteImgError) {
+      return sendErrorResponse(
+        `Product id: ${id} does not exist or image could not be deleted: ${deleteImgError.message}`,
+      );
+    }
+  }
+
+  const { error } = await supabase.from("products").delete().eq("id", id);
 
   if (error) return sendErrorResponse(error.message);
 
